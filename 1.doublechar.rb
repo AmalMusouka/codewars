eff4c2248918fbb4743ac3290d04fb4182e5ffbe
amalmusouka <amalsudhikumar20@gmail.com>
2024-07-23 15:18:05 +0530

    feat: better solution for doublechar

--- 1.doublechar.rb
+++ 1.doublechar.rb
@@ -3,11 +3,14 @@
 
 
 def doublechar(s)
-  a = []
-  s.each_char do |c|
-    a = a.append(c).append(c)
-  end
-  pp a.join
+#   a = []
+#   s.each_char do |c|
+#     a = a.append(c).append(c)
+#   end
+#   pp a.join
+
+pp s.chars.map{|c| c*2}.join
+
 end
 
 
