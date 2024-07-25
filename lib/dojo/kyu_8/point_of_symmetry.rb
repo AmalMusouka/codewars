725e7e7cd3feda7809aba0599242b27036ef6510
amalmusouka <amalsudhikumar20@gmail.com>
2024-07-25 15:51:45 +0530

    feat: Improved solution for point of symmetry which requires use of matrix

--- lib/dojo/kyu_8/point_of_symmetry.rb
+++ lib/dojo/kyu_8/point_of_symmetry.rb
@@ -5,6 +5,8 @@ module Dojo
     module PointOfSymmetry
       def self.symmetry_point(p,q)
         return [2*q[0]-p[0],2*q[1]-p[1]]
+        #require Matrix
+        # (2 * Vector[*q] - Vector[*p]).to_a
       end
     end
   end
