d0e4d19bab23abe067573deeffcefbf14ebb7736
amalmusouka <amalsudhikumar20@gmail.com>
2024-07-27 19:05:55 +0530

    refactor: typo

--- spec/dojo/kyu_8/dynamic_method_calls_spec.rb
+++ spec/dojo/kyu_8/dynamic_method_calls_spec.rb
@@ -2,10 +2,10 @@
 
 RSpec.describe Dojo::Kyu8::DynamicMethodCalls do
   describe '#dynamic_caller' do
-    it 'returns which quarter of the year the given month is' do
+    it 'returns the output after dynamically calling the method' do
       expect(described_class.dynamic_caller('zaphod beeblebrox', :upcase)).to eq('ZAPHOD BEEBLEBROX')
     end
-    it 'returns which quarter of the year the given month is' do
+    it 'returns the output after dynamically calling the method' do
       expect(described_class.dynamic_caller(-42, :abs)).to eq(42)
     end
   end
