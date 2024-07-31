6616429e4b54ed92985d8280952b78957145e8e2
amalmusouka <amalsudhikumar20@gmail.com>
2024-07-31 17:19:10 +0530

    feat(kyu7): improved solution for Vowel count

--- lib/dojo/kyu_7/vowel_count.rb
+++ lib/dojo/kyu_7/vowel_count.rb
@@ -4,10 +4,11 @@ module Dojo
   module Kyu7
     module VowelCount
       def self.get_count(input_str)
-        h = { 'a' => 1, 'e' => 2, 'i' => 3, 'o' => 4, 'u' => 5 }
-        i = 0
-        input_str.each_char { |c| h.key?(c) ? i += 1 : next }
-        i
+        input_str.count('aeiou')
+        # h = { 'a' => 1, 'e' => 2, 'i' => 3, 'o' => 4, 'u' => 5 }
+        # i = 0
+        # input_str.each_char { |c| h.key?(c) ? i += 1 : next }
+        # i
       end
     end
   end
