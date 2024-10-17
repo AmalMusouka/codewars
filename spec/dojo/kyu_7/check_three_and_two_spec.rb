171001fd82fa5f7f3b93ec2b2ea9acf0afa9a925
amalmusouka <amalsudhikumar20@gmail.com>
2024-10-17 11:01:26 +0200

    feat(kyu7): Solution for check three and two

--- spec/dojo/kyu_7/check_three_and_two_spec.rb
+++ spec/dojo/kyu_7/check_three_and_two_spec.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
 
 RSpec.describe Dojo::Kyu7::CheckThreeAndTwo do
   describe '#check_three_and_two' do
-    it 'returns whether or not the given pairs have 1s in the same bit location' do
+    it 'returns whether the given string has 3 of one character and 2 of another' do
       expect(described_class.check_three_and_two(["a", "a", "a", "b", "b"])).to eq(true)
     end
-    it 'returns whether or not the given pairs have 1s in the same bit location' do
+    it 'returns whether the given string has 3 of one character and 2 of another' do
       expect(described_class.check_three_and_two(["a", "c", "a", "c", "b"])).to eq(false)
     end
-    it 'returns whether or not the given pairs have 1s in the same bit location' do
+    it 'returns whether the given string has 3 of one character and 2 of another' do
       expect(described_class.check_three_and_two(["a", "a", "a", "a", "a"])).to eq(false)
     end
   end
